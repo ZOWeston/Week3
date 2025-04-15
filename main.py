2c089ac4212fa323de9b5a7571e370937eb7201d
ZOWeston <zoneoutalternate@gmail.com>
2025-04-14 20:00:13 -0700

    evil gang

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import numpy as np
 array = np.random.randint(1,10, size=(5,5), dtype=int)
 print(array)
 print(array[1,2])
+print(array.sum())
+print(np.mean(array, axis=1))
+print(np.max(array, axis=0))
